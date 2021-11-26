f7ba386c2275a7edcc212e4d5b3b6ef5b6bfcc67
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-11-26 17:03:23 +0500

    Complete Header testing

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Header from "../components/Header";
 
 test("header renders", () => {
   expect(render(<Header />)).toMatchSnapshot();
+  screen.getByRole("heading");
+  screen.debug();
 });
 
 test("header renders correct title", () => {
-  const { getByText } = render(<Header />);
+  const { getByText, queryByText } = render(<Header />);
   expect(getByText(/Memory Card/)).toBeInTheDocument();
+  expect(queryByText(/Memory Card/)).toBeInTheDocument();
+});
+
+test("header and h1 have correct class", () => {
+  const { container, getByText } = render(<Header />);
+  expect(container.firstChild).toHaveClass("header");
+  expect(getByText(/Memory/)).toHaveClass("title");
 });
