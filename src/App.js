5f73517765a2949e2f79a577d46077099a929bfe
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-11-26 17:39:28 +0500

    Test App for Scoreboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Scoreboard from "./components/Scoreboard";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Scoreboard currentScore={null} bestScore={null} />
     </div>
   );
 }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 
 test("App renders", () => {
   const { container } = render(<App />);
+
+  screen.debug();
+
   expect(container).toMatchSnapshot();
   expect(container.firstChild).toHaveClass("App");
 });
@@ -13,4 +16,10 @@ test("header renders", () => {
   const { container } = render(<App />);
   const header = container.querySelector(".header");
   expect(header).toBeInTheDocument();
+});
+
+test("scoreboard renders", () => {
+  const { container } = render(<App />);
+  const scoreboard = container.querySelector(".scoreboard");
+  expect(scoreboard).toBeInTheDocument();
 });
