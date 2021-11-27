05f2b96412c33a2b54718414968d301c7eeddf62
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-11-27 20:08:14 +0500

    Test ball section's re-render

--- src/tests/BallSection.test.js
+++ src/tests/BallSection.test.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import BallSection from "../components/BallSection";
 
 test("renders ball section", () => {
-  expect(render(<BallSection />)).toMatchSnapshot();
+  const { container } = render(<BallSection />);
+  expect(container).toMatchSnapshot();
+  expect(container.firstChild).toHaveClass("ball-section");
+});
+
+test("re-renders ball divs", () => {
+  const { container } = render(<BallSection />);
+  expect(container).toMatchSnapshot();
   screen.debug();
+
+  const div = container.querySelector("div");
+  userEvent.click(div);
+  screen.debug();
+  expect(container).toMatchSnapshot();
 });
