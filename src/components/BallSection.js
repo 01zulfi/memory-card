eafc36591a6bd3b68c2a8cccbea5e61277d8666e
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-11-28 18:25:41 +0500

    Complete BallSection component

--- src/components/BallSection.js
+++ src/components/BallSection.js
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
 import { balls } from "../Balls";
 
-const BallSection = () => {
+const BallSection = ({ score }) => {
   const [fourBalls, setFourBalls] = useState(balls.getFourRandom());
 
-  const clickHandler = () => {
+  const toggleFailureBorder = (element) => {
+    element.style.border = "2px solid red";
+  };
+
+  const setBallClicked = (id, event) => {
+    const targetBall = balls.find(id);
+
+    if (targetBall.clicked) {
+      score.failure();
+      toggleFailureBorder(event.target);
+      balls.setAllClickedFalse();
+      setTimeout(() => {
+        setFourBalls(balls.getFourRandom());
+      }, 1000);
+      return;
+    }
+
     setFourBalls(balls.getFourRandom());
+    score.success();
+    targetBall.setClickedTrue();
+  };
+
+  const clickHandler = (event) => {
+    const id = Number(event.target.getAttribute("data-id"));
+    setBallClicked(id, event);
   };
 
   return (
     <section className="ball-section">
       {fourBalls.map((ball) => (
-        <div onClick={clickHandler} key={ball.id}>
+        <div
+          data-id={ball.id}
+          className="ball-div"
+          onClick={clickHandler}
+          key={ball.id}
+        >
           {ball.name}
         </div>
       ))}
