909ffc489c67de2c964d949ada48f773bb7bafe8
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-12-01 19:13:15 +0500

    Change title & add game desc

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "../styles/Header.css";
 
 const Header = () => {
   return (
     <div className="header">
-      <h1 className="title">Memory Card</h1>
+      <h1 className="title">Balls: A Memory Card Game</h1>
+      <p>Click a ball to get a point (but don't click a ball twice!)</p>
     </div>
   );
 };
